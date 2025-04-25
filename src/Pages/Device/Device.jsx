1203c88054890ae00533c8cb534ea3313ae8bf23
sandali <sandaliwathsalavi25@gmail.com>
2025-04-26 04:21:15 +0530

    notification one added

--- src/Pages/Device/Device.jsx
+++ src/Pages/Device/Device.jsx
@@ -3,6 +3,18 @@ import Sidebar from '../../Component/Sidebar/Sidebar';
 import Topbar from '../../Component/Topbar/Topbar';
 import Footer from '../../Component/Footer/Footer';
 import './Device.css';
+import { FaBellSlash, FaTrash } from 'react-icons/fa';
+
+const notifications = [
+  {
+    id: 1,
+    name: 'Brigid Dawson',
+    action: 'followed you',
+    daysAgo: '4 days ago',
+    image: '/assets/avatar.png'
+  },
+ 
+];
 
 const Device = () => {
   return (
@@ -11,13 +23,23 @@ const Device = () => {
       <div className="main-container d-flex">
         <Sidebar />
         <div className="device-content container">
-          <h2 className="my-4">--Notifications--</h2>
-          <div className="card shadow p-4">
-            <form>
-              
-
-             
-            </form>
+          <h2 className="notification-title">--Notifications--</h2>
+          <div className="notification-wrapper">
+            {notifications.map(notification => (
+              <div className="notification-card" key={notification.id}>
+                <div className="notification-avatar">
+                  <img src={notification.image} alt="user" />
+                </div>
+                <div className="notification-text">
+                  <strong>{notification.name}</strong> {notification.action}
+                  <div className="notification-time">{notification.daysAgo}</div>
+                </div>
+                <div className="notification-actions">
+                  <FaBellSlash className="icon-btn" title="Mute" />
+                  <FaTrash className="icon-btn" title="Delete" />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
