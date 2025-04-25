8a6066ddff8760c16d77313519c6426c8fbeeffd
sandali <sandaliwathsalavi25@gmail.com>
2025-04-26 02:17:23 +0530

    add booster section for achievement process section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
                     { title: "Learning", value: "Level Up...", image: "humidity.jpg" },
                     { title: "Posts", value: "Hot Take...", image: "soil-moisture.jpg" },
                     { title: "Learning Plans", value: "Smart Growth...", image: "co2-level.png" },
-                    { title: "Achievement Process", value: "", image: "electrical-conductivity.jpg" },
+                    { title: "Achievement Process", value: "Elite Progress...", image: "electrical-conductivity.jpg" },
                     { title: "User EXperience", value: "", image: "o2-level.jpg" }].map((card, index) => (
                     <div className="col-md-3" key={index}>
                       <div className="card h-100 shadow-sm text-center">
