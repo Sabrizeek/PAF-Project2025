e0793af293fba96f4af6b5a1d85ff032d9da06fc
sandali <sandaliwathsalavi25@gmail.com>
2025-04-26 02:13:43 +0530

    add booster section for content delivery section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -83,7 +83,7 @@ const Home = () => {
 
                 <div className="dashboard-cards row g-3 mt-3">
                   {/* Cards for Tunnel 01 */}
-                  {[{ title: "Content Delivery", value: "", image: "temperature.jpg" },
+                  {[{ title: "Content Delivery", value: "Uncover Secrets...", image: "temperature.jpg" },
                     { title: "Learning", value: "", image: "humidity.jpg" },
                     { title: "Posts", value: "", image: "soil-moisture.jpg" },
                     { title: "Learning Plans", value: "", image: "co2-level.png" },
