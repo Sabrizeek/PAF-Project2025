1574ad7c15ca8ee28ab994f6fc794804ced920b0
Pamod Pushpamal <145574206+PamodN@users.noreply.github.com>
2025-04-27 23:43:59 +0530

    create app

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AddLearningPlan from "./Pages/LearningPlan/AddLearningPlan";
+import AllLearningPlan from "./Pages/LearningPlan/AllLearningPlan";
+import UpdateLearningPlan from "./Pages/LearningPlan/UpdateLearningPlan";
+import MyLearningPlan from "./Pages/LearningPlan/MyLearningPlan";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/addLearningPlan" element={<AddLearningPlan />} />
+      <Route path="/allLearningPlan" element={<AllLearningPlan />} />
+      <Route path="/myLearningPlan" element={<MyLearningPlan />} />
+      <Route path="/updateLearningPlan/:id" element={<UpdateLearningPlan />} />
+    </Routes>
+  );
+}
+
+export default App;
